b17d156d569e6167dc210b1094b45e251a1040ba
Adrián Márquez <adrianmarquez1607@gmail.com>
2026-06-11 10:10:00 -0400

    Configuración inicial del proyecto
    Se ha añadido un archivo "main.cpp" el cuál mostrará un mensaje de bienvenida al sistema

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "===============================================" << std::endl;
+    std::cout << "   Bienvenido al Sistema de Logistica Logiix   " << std::endl;
+    std::cout << "===============================================" << std::endl;
+    std::cout << "\nEstado: Configuracion inicial completada." << std::endl;
+    std::cout << "Listo para la implementacion de estructuras de datos." << std::endl;
+
+    return 0;
+}
